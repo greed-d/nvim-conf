8dcdf7fe42da623c09d4451b740cb80d88458c53
greeid <deekshit.bti@gmail.com>
2024-06-25 10:05:46 +0545

    `add` : indenting options

--- lua/options.lua
+++ lua/options.lua
@@ -18,11 +18,11 @@ o.cursorlineopt = 'both'
 o.termguicolors = true
 
 -- Indenting
--- o.expandtab = true
--- o.shiftwidth = 2
--- o.smartindent = true
--- o.tabstop = 2
--- o.softtabstop = 2
+o.expandtab = true
+o.shiftwidth = 2
+o.smartindent = true
+o.tabstop = 2
+o.softtabstop = 2
 
 o.winbar = '%f %m'
 
