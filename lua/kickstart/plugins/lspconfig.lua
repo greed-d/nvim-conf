600301b4ad1704b8c8e032094129c3a0221c4cab
greeid <deekshit.bti@gmail.com>
2024-06-29 18:05:46 +0545

    `chore` : change gitsigns

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -173,6 +173,7 @@ return {
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         clangd = {},
+        cssls = {},
         -- gopls = {},
         pyright = {},
         bashls = {},
