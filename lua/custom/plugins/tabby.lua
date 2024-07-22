69f78e67b7600d2aa9aaa92e5257e32fe10d88a6
greeid <deekshit.bti@gmail.com>
2024-07-22 23:22:12 +0545

    `feat` : show diagnostic in bufferline

--- lua/custom/plugins/tabby.lua
+++ lua/custom/plugins/tabby.lua
@@ -1,12 +1,31 @@
 return {
   'akinsho/bufferline.nvim',
-  event = 'VeryLazy',
+  event = 'BufRead',
   version = '*',
   dependencies = 'nvim-tree/nvim-web-devicons',
+
   config = function()
     require('bufferline').setup {
       options = {
         separator_style = 'slant',
+        diagnostics = 'nvim_lsp',
+        always_show_bufferline = false,
+        diagnostics_indicator = function(_, _, diag)
+          local icons = {
+            Error = ' ',
+            Warn = ' ',
+          }
+          local ret = (diag.error and icons.Error .. diag.error .. ' ' or '') .. (diag.warning and icons.Warn .. diag.warning or '')
+          return vim.trim(ret)
+        end,
+        offsets = {
+          {
+            filetype = 'neo-tree',
+            text = 'Neo-tree',
+            highlight = 'Directory',
+            text_align = 'left',
+          },
+        },
       },
     }
   end,
