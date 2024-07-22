ba56e1265686cf43cc06f73f7e3fd5e1ce076166
greeid <deekshit.bti@gmail.com>
2024-07-22 23:23:20 +0545

    `add` : move from ibl to indentmini

--- lua/kickstart/plugins/indent_line.lua
+++ lua/kickstart/plugins/indent_line.lua
@@ -1,15 +1,29 @@
+-- return {
+--   'lukas-reineke/indent-blankline.nvim',
+--   event = 'BufReadPre',
+--   main = 'ibl',
+--   opts = {
+--     indent = { char = '│' },
+--     scope = {
+--       show_start = false,
+--       show_end = false,
+--     },
+--     exclude = {
+--       buftypes = { 'terminal', 'nofile' },
+--     },
+--   },
+-- }
 return {
-  'lukas-reineke/indent-blankline.nvim',
-  event = 'BufReadPre',
-  main = 'ibl',
-  opts = {
-    indent = { char = '│' },
-    scope = {
-      show_start = false,
-      show_end = false,
-    },
-    exclude = {
-      buftypes = { 'terminal', 'nofile' },
-    },
-  },
+  'nvimdev/indentmini.nvim',
+  event = 'User FilePost',
+  config = function()
+    require('indentmini').setup {
+      char = '│',
+      -- Colors are applied automatically based on user-defined highlight groups.
+      -- There is no default value.
+      vim.cmd.highlight 'IndentLine guifg=#545560',
+      -- Current indent line highlight
+      vim.cmd.highlight 'IndentLineCurrent guifg=#A0A8CD',
+    }
+  end,
 }
