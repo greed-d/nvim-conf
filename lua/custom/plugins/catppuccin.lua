71ca238130a0e4dfc3d34eaa17e0cb17e118a516
greed-d <deekshit.bti@gmail.com>
2024-10-30 18:53:05 +0545

    `chore` : enable catppuccin

--- lua/custom/plugins/catppuccin.lua
+++ lua/custom/plugins/catppuccin.lua
@@ -1,13 +1,12 @@
 return {
-  "catppuccin/nvim",
-  name = "catppuccin",
-  priority = 1000, 
+  'catppuccin/nvim',
+  name = 'catppuccin',
+  priority = 1000,
   config = function()
     vim.cmd.colorscheme 'catppuccin'
-    require("catppuccin").setup({ flavour = "mocha" })
+    require('catppuccin').setup { flavour = 'mocha' }
 
-    vim.api.nvim_set_hl(0, "GitSignsDelete", { fg = '#f38ba8'})
-    vim.api.nvim_set_hl(0, "GitSignsAdd", { fg = '#a6e3a1'})
-  end
-
+    vim.api.nvim_set_hl(0, 'GitSignsDelete', { fg = '#f38ba8' })
+    vim.api.nvim_set_hl(0, 'GitSignsAdd', { fg = '#a6e3a1' })
+  end,
 }
